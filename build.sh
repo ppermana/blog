e14f6c276330650c5bd819582db4b85593cf3a21
Pahri <pahri.personal@gmail.com>
2025-03-29 14:15:21 +0700

    trying fixing theme and rst; gtp-4o mini reccom.

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Convert reST files to HTML
+for file in content/**/*.rst; do
+    rst2html.py "$file" "${file%.rst}.html"
+done
+
+# Run Hugo to build the site with options
+hugo --gc --minify

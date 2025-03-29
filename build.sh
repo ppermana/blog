ae9c8893d69bdf1303b29940a72a005700857551
Pahri <pahri.personal@gmail.com>
2025-03-29 14:27:41 +0700

    still fixing

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Convert reST files to HTML
-for file in content/**/*.rst; do
-    rst2html.py "$file" "${file%.rst}.html"
-done
-
-# Run Hugo to build the site with options
-hugo --gc --minify
